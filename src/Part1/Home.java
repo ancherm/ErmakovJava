357f5022a3042598b6ee3e33e41326e88be514a3
Ancherm <andrewcherm2014@gmail.com>
2023-08-21 17:09:18 +0400

    Решена задача 1.1.5

---
+++ src/Part1/Home.java
@@ -0,0 +1,28 @@
+package Part1;
+
+public class Home {
+    int countFloor;
+
+    public Home(int countFloor) {
+        this.countFloor = countFloor;
+    }
+
+    public String toPrintString() {
+        if (countFloor == 1) {
+            return "Дом с " + countFloor + " этажом";
+        }
+        else {
+            return "Дом с " + countFloor + " этажами";
+        }
+    }
+
+    public static void main(String[] args) {
+        Home home1 = new Home(1);
+        Home home2 = new Home(5);
+        Home home3 = new Home(23);
+
+        System.out.println(home1.toPrintString());
+        System.out.println(home2.toPrintString());
+        System.out.println(home3.toPrintString());
+    }
+}
