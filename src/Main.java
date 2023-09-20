1f76a0277ff45e427f75fa32f1b2c597dbf96672
Ancherm <andrewcherm2014@gmail.com>
2023-09-20 12:41:32 +0400

    1.1 с конструктором

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import Part1.Point;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1.1.1 Точка
+        Point point1 = new Point(1, 0);
+        Point point2 = new Point(-4, 2);
+        Point point3 = new Point(8, -3);
+
+        System.out.println(point1);
+        System.out.println(point2);
+        System.out.println(point3);
+        System.out.println();
+    }
+
+
+}
--- src/Part1/Point.java
+++ src/Part1/Point.java
@@ -8,18 +8,7 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-
     public String toString() {
         return "{ " + x + "; " + y + " }";
-    }
-
-    public static void main(String[] args) {
-        Point point1 = new Point(1, 0);
-        Point point2 = new Point(-4, 2);
-        Point point3 = new Point(8, -3);
-
-        System.out.println(point1);
-        System.out.println(point2);
-        System.out.println(point3);
     }
 }
