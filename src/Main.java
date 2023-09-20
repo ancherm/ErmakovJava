f2e86143dec5b86c563a191b4498d7c3ad0d505d
Ancherm <andrewcherm2014@gmail.com>
2023-09-20 12:44:00 +0400

    1.2 с конструктором

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Part1.Person;
 import Part1.Point;
 
 public class Main {
@@ -7,9 +8,21 @@ public class Main {
         Point point2 = new Point(-4, 2);
         Point point3 = new Point(8, -3);
 
+        System.out.println("1.1.1 Точка");
         System.out.println(point1);
         System.out.println(point2);
         System.out.println(point3);
+        System.out.println();
+
+        // 1.1.2 Человек
+        Person person1 = new Person("Клеопатра", 152);
+        Person person2 = new Person("Пушкин", 167);
+        Person person3 = new Person("Владимир", 189);
+
+        System.out.println("1.1.2 Человек");
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
         System.out.println();
     }
 
--- src/Part1/Person.java
+++ src/Part1/Person.java
@@ -13,13 +13,4 @@ public class Person {
         return "Человек с именем " + name + " и ростом " + height;
     }
 
-    public static void main(String[] args) {
-        Person person1 = new Person("Клеопатра", 152);
-        Person person2 = new Person("Пушкин", 167);
-        Person person3 = new Person("Владимир", 189);
-
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
-    }
 }
