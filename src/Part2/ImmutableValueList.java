13ff031148b655d380bade294f09d83b42d0b975
Ancherm <andrewcherm2014@gmail.com>
2023-10-19 18:02:14 +0400

    Test commit2

---
+++ src/Part2/ImmutableValueList.java
@@ -0,0 +1,18 @@
+package Part2;
+
+import java.util.Arrays;
+
+public class ImmutableValueList {
+    private int[] array;
+
+    //???
+//    public ImmutableValueList(int[] array) {
+//        this.array = array.clone();
+//    }
+//    public ImmutableValueList(int... numbers) {
+//        this(numbers);
+//    }
+//    public ImmutableValueList(ImmutableValueList array) {
+//        this(array.array);
+//    }
+}
