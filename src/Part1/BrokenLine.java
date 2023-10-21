202e03607e6544562937cb054c2d3f23e1466931
Ancherm <andrewcherm2014@gmail.com>
2023-10-22 03:06:09 +0400

    3.1.1

--- src/Part1/BrokenLine.java
+++ src/Part1/BrokenLine.java
@@ -6,26 +6,41 @@ import java.util.List;
 public class BrokenLine {
     private List<Point> pointList = new ArrayList<>();
 
-    public BrokenLine(){}
+    public BrokenLine() {
+    }
+
     public BrokenLine(List<Point> pointList) {
         addPoints(pointList);
     }
-    public BrokenLine(Point...pointList) {
+
+    public BrokenLine(Point... pointList) {
         this(List.of(pointList));
     }
+
+
+    public List<Point> getPointList() {
+        return new ArrayList<>(pointList);
+    }
+
 
     public void addPoints(List<Point> pointList) {
         this.pointList.addAll(pointList);
     }
 
+
     public double lengthOfBrokenLine() {
+        return lengthOfBrokenLine(pointList);
+    }
+
+    protected double lengthOfBrokenLine(List<Point> pointList) {
         double length = 0;
         for (int i = 0; i < pointList.size() - 1; i++) {
-            length += Math.sqrt(Math.pow(pointList.get(i+1).getX() - pointList.get(i).getX(), 2) +
-                    Math.pow(pointList.get(i+1).getY() - pointList.get(i).getY(), 2));
+            length += Math.sqrt(Math.pow(pointList.get(i + 1).getX() - pointList.get(i).getX(), 2) +
+                    Math.pow(pointList.get(i + 1).getY() - pointList.get(i).getY(), 2));
         }
         return length;
     }
+
     public void moveLastPoint(Point point) {
         pointList.set(pointList.size() - 1, new Point(point.getX(), point.getY()));
     }
---
+++ src/Part3/ClosedBrokenLine.java
@@ -0,0 +1,34 @@
+package Part3;
+
+import Part1.BrokenLine;
+import Part1.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClosedBrokenLine extends BrokenLine {
+
+    public ClosedBrokenLine(List<Point> points) {
+        super(points);
+    }
+
+    @Override
+    public double lengthOfBrokenLine() {
+        List<Point> closedPointList = getPointList();
+        closedPointList.add(getPointList().get(0));
+
+        return lengthOfBrokenLine(closedPointList);
+//        double length = 0;
+//        for (int i = 0; i < getPointList().size() - 1; i++) {
+//            length += Math.sqrt(Math.pow(getPointList().get(i+1).getX() - getPointList().get(i).getX(), 2) +
+//                    Math.pow(getPointList().get(i+1).getY() - getPointList().get(i).getY(), 2));
+//        }
+//        return length;
+    }
+
+    public void printList() {
+        for (Point point : getPointList()) {
+            System.out.println(point);
+        }
+    }
+}
