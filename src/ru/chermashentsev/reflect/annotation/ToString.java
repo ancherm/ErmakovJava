18429157555ca8174c94058c91614eedea44fbfa
Ancherm <andrewcherm2014@gmail.com>
2024-02-28 02:22:32 +0400

    edit annotations

--- src/ru/chermashentsev/reflect/annotation/ToString.java
+++ src/ru/chermashentsev/reflect/annotation/ToString.java
@@ -2,9 +2,12 @@ package ru.chermashentsev.reflect.annotation;
 
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ElementType.FIELD, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ToString {
     boolean value() default true;
 }
--- src/ru/chermashentsev/reflect/annotation/Validate.java
+++ src/ru/chermashentsev/reflect/annotation/Validate.java
@@ -1,10 +1,13 @@
 package ru.chermashentsev.reflect.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
 @Target({ElementType.ANNOTATION_TYPE, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Validate {
     Class[] value();
 }
